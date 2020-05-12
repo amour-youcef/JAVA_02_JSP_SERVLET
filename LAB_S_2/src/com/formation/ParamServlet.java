16323a07e95b724c990377b46b1021f9ffa6b971
dev <amour-youcef@hotmail.com>
2020-05-12 07:15:34 +0200

    12052020

--- LAB_S_2/src/com/formation/ParamServlet.java
+++ LAB_S_2/src/com/formation/ParamServlet.java
@@ -45,7 +45,6 @@ public class ParamServlet extends HttpServlet {
 		
 	}
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
